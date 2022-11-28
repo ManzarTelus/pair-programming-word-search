218c79ba99db191e82e2ea92c63fabe6f4a73226
Manzar <manzar.hussain@telus.com>
2022-11-28 04:00:22 -0500

    Pair Programming - Word Search Challenge

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,44 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  if (letters.length < 1 || word.length < 1) return false;
+    
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
     }
-}
+  }
 
-module.exports = wordSearch
+  //Transposing the matrix as doen in last pair Assignment
+  const verticalMatrix = [];
+
+  for (let i = 0; i < letters[0].length; i++) {
+    verticalMatrix.push([]);
+  }
+  for (let row = 0; row < letters.length; row++) {
+    for (let col = 0; col < letters[row].length; col++) {
+      verticalMatrix[col][row]  = letters[row][col];
+    }
+  }
+            
+  const verticalJoin = verticalMatrix.map(ls => ls.join(''));
+  for (const l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+        
+  return false;
+};
+
+
+module.exports = wordSearch;
+
+/*
+****Output****
+  #wordSearch()
+    ✓ should return false if the word is not present
+    ✓ should return true if the word is present
+    ✓ should return true if the word is present vertically
+    ✓ should return false if word is an empty string
+
+
+  4 passing (4ms)
+  */
